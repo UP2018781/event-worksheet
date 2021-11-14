d9cc93e9d7c9e7d2b8b818e6d74758ec980cd698
UP2018781 <91746177+UP2018781@users.noreply.github.com>
2021-11-14 19:30:40 +0000

    finished test 4

--- ws_events/index.js
+++ ws_events/index.js
@@ -8,3 +8,29 @@ function tttcAttacher(){
     const button = document.querySelector('#button0');
     button.addEventListener('click',targetTextToConsole);
 }
+
+function lovelyParaAttacher(){
+    const thisisalovelypara = document.querySelector('#thisisalovelyparagraph');
+    thisisalovelypara.addEventListener('click',lovelyToggle);
+}
+
+function lovelyButtonAttacher(){
+    const button1 = document.querySelector('#button1');
+    button1.addEventListener('click',lovelyToggle);
+}
+
+function concatAttacher(){
+    const in1 = document.querySelector('#in1');
+    const in2 = document.querySelector('#in2');
+    in1.addEventListener('change',out1Handler); //can be (and should be) 'input' not 'change' i reckon
+    in2.addEventListener('change',out1Handler);
+
+}
+
+function out1Handler(){
+    const out1 = document.querySelector('#out1');
+    const in1 = document.querySelector('#in1');
+    const in2 = document.querySelector('#in2');
+    console.log(in1.value,in2.value);
+    out1.textContent = (in1.value+in2.value);
+}
